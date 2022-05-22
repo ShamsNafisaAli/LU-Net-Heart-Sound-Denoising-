ac08a3d12e0c10810049799262c840ec037c9fb0
ShamsNafisaAli <65068500+ShamsNafisaAli@users.noreply.github.com>
2022-05-22 22:27:06 +0600

    Update config.py

--- Codes/config.py
+++ Codes/config.py
@@ -1,14 +1,14 @@
-path_Heart_Train = "./data_set/physionet_Langley+Spectral_Audio/train"
-path_Lung_Train = "./data_set/lung_sound_dataset/train"
+path_Heart_Train="/content/drive/MyDrive/Heart Sound Denoising All Data/PHS Data (Processed)/train"
 
-pathheartVal ='./data_set/Git_hub_dataset/Git_val'
-pathlungval ='./data_set/lung_sound_dataset/val'
-pathhospitalval ='./data_set/Hospital_noise_filtered_resampled/train'
+path_Lung_Train="/content/drive/MyDrive/Heart Sound Denoising All Data/ICBHI Dataset (Processed)/train"
 
-window_size = 0.8
-name_model = "lunet"
-input_shape = 800
-output_shape = 800
-sampling_rate_new = 1000
-check = rf"./check_points/{name_model}.h5"
+pathheartVal='/content/drive/MyDrive/Heart Sound Denoising All Data/OAHS Dataset/Git_val'
+pathlungval='/content/drive/MyDrive/Heart Sound Denoising All Data/ICBHI Dataset (Processed)/val'
+pathhospitalval='/content/drive/MyDrive/Heart Sound Denoising All Data/Hospital Ambient Noise (HAN) Dataset'
+window_size=.8
+name_model="lunet"
+input_shape=800
+output_shape=800
+sampling_rate_new=1000
+check=rf"/content/drive/MyDrive/Heart Sound Denoising All Data/{name_model}_model_pc07_PhysioNet_{sampling_rate_new}_-3dB_to_6dB.h5"
 #check=rf"./check_points/{name_model}_model_pc07_PhysioNet_{sampling_rate_new}_-3dB_to_6dB.h5"
