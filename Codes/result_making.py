72adb475330b40c6b53786ee77fd5856f7d0dbfd
samiulshuvo <74701331+samiulshuvo@users.noreply.github.com>
2022-05-22 21:10:52 +0600

    Update result_making.py

--- Codes/result_making.py
+++ Codes/result_making.py
@@ -6,5 +6,5 @@ from config import *
 from utilis import *
 model=keras.models.load_model('check_points/lunet.h5')
 for snr in [-6,-3,0,3,6]:
-    XtestL,YtestL,label1= get_files_and_resampleP(1000,0.8,locH=pathheartVal,locN=pathhospitalval, db_SNR =snr,mode=1)
+    XtestL,YtestL,label1= get_files_and_resampleP(1000,0.8,locH=pathheartVal,locN=pathhospitalval, db_SNR =snr,mode=1) ## for lung sound noise locN=pathlungval
     est_testL=check_SNR_non_merged(XtestL,YtestL,snr,model)
