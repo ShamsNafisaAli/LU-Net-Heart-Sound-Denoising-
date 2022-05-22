76bdc8374755cdd4811b71a9a4f69eb05a2c4ea3
ShamsNafisaAli <65068500+ShamsNafisaAli@users.noreply.github.com>
2022-05-22 19:21:37 +0600

    Create config.py

---
+++ Codes/config.py
@@ -0,0 +1,14 @@
+path_Heart_Train = "./data_set/physionet_Langley+Spectral_Audio/train"
+path_Lung_Train = "./data_set/lung_sound_dataset/train"
+
+pathheartVal ='./data_set/Git_hub_dataset/Git_val'
+pathlungval ='./data_set/lung_sound_dataset/val'
+pathhospitalval ='./data_set/Hospital_noise_filtered_resampled/train'
+
+window_size = 0.8
+name_model = "lunet"
+input_shape = 800
+output_shape = 800
+sampling_rate_new = 1000
+check = rf"./check_points/{name_model}_model_pc07_PhysioNet_{sampling_rate_new}_-3dB_to_6dB.h5"
+#check=rf"./check_points/{name_model}_model_pc07_PhysioNet_{sampling_rate_new}_-3dB_to_6dB.h5"
