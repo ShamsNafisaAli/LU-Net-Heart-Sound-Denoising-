6f17c30ac1c149aeee9ab2b121cdb27c6b1b977e
ShamsNafisaAli <65068500+ShamsNafisaAli@users.noreply.github.com>
2022-05-22 19:39:52 +0600

    Create result_making.py

---
+++ Codes/result_making.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+from tensorflow import keras
+from processing_initial import get_files_and_resample
+
+from config import *
+from utilis import *
+model=keras.models.load_model('check_points/unet2d_model_pc07_PhysioNet_1000_-3dB_to_6dB.h5')
+for snr in [-6,-3,0,3,6]:
+    XtestL,YtestL,label1= get_files_and_resampleP(1000,0.8,locH=pathheartVal,locN=pathhospitalval, db_SNR =snr,mode=1)
+
+    est_testL=check_SNR_non_merged(XtestL,YtestL,snr,model)
