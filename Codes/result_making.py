3f3c8f5182c2fdeb7699bb8344f08a865af26f69
ShamsNafisaAli <65068500+ShamsNafisaAli@users.noreply.github.com>
2022-05-23 00:15:06 +0600

    Update result_making.py

--- Codes/result_making.py
+++ Codes/result_making.py
@@ -7,7 +7,7 @@ from utils import *
 import pandas as pd
 
 #Use the pretrained weights from Models folder or use your own check point's directory if you train by yourself
-model = keras.models.load_model('Models/LU-Net.h5') 
+model = keras.models.load_model('LU-Net.h5') 
 # Result making step for Open access heart sound dataset
 for snr in [-6,-3, 0,3,6]:
     XtestL,YtestL,label1= get_files_and_resample(1000, 0.8, locH = pathheartVal,locN = pathhospitalval, db_SNR = snr, mode = 1)
