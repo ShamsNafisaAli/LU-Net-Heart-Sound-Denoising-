af54d2535ce22bfb32d7943d705119c301a4a6b8
ShamsNafisaAli <65068500+ShamsNafisaAli@users.noreply.github.com>
2022-05-22 23:09:04 +0600

    Update result_making.py

--- Codes/result_making.py
+++ Codes/result_making.py
@@ -4,7 +4,7 @@ from processing_initial import get_files_and_resample
 from config import *
 from utils import *
 
-model = keras.models.load_model('check_points/lunet.h5')
+model = keras.models.load_model('Models/LU-Net.h5')  #Use the saved weight or use your own check point's directory if you train by yourself
 for snr in [-6,-3,0,3,6]:
     XtestL,YtestL,label1= get_files_and_resampleP(1000,0.8,locH=pathheartVal,locN=pathhospitalval, db_SNR =snr,mode=1)
     est_testL=check_SNR_non_merged(XtestL,YtestL,snr,model)
